8a45a3c652e44824b9e3b22def5f65dfcb3efffd
erinldugan <erinldugan@gmail.com>
2018-08-03 14:53:02 -0400

    scrape all items for one page

---
+++ petfinder1.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+
+# Start page to scrape
+#driver.get("https://www.petfinder.com/search/dogs-for-adoption/us/ny/white-plains/?distance=10&sort%5B0%5D=recently_added")
+driver.get("https://www.petfinder.com/search/dogs-for-adoption/us/pa/state-college/?distance=10&sort%5B0%5D=recently_added")
+
+#STEPS:
+# 1 - From search result page - Click dog's pic (pet_tile) to go to its page
+# 2 - Scrape individual pages
+# 3 - Click "Next Pet"
+# 4 - Repeat 2-3 until no more pages
+
+pet_tile = driver.find_element_by_xpath('//a[@class="petCard-overlay-link"]').get_attribute("href")
+driver.get(pet_tile)
+
+index = 1  #First pet
+while True:  #until no Next button appears
+    try:
+        print("Scraping Pet # " + str(index))
+        index = index + 1
+        # Find all the info on the page
+        pet_dict = {}
+
+        name = driver.find_element_by_xpath('.//h1[@id="Detail_Main"]').text
+        organization = driver.find_element_by_xpath('.//pf-truncate[@line-count="3"]').text
+
+        pet_dict['name'] = name
+        pet_dict['organization'] = organization
+        print(pet_dict.values())
+
+        next_pet = driver.find_element_by_xpath('(.//a[@pf-ensighten])[2]').get_attribute("href")  #bad link
+        driver.get(next_pet)
+
+    except Exception as e:
+        print(e)
+        driver.close()
+        break
--- petfinder_startup.py
+++ petfinder_startup.py
@@ -2,17 +2,27 @@ from selenium import webdriver
 import time
 
 driver = webdriver.Chrome()
-# Go to the page that we want to scrape
-#driver.get("https://www.petfinder.com/search/dogs-for-adoption/us/ny/white-plains/")
 
-#FOR SCRAPING 1ST PET PAGE:
-driver.get("https://www.petfinder.com/dog/brody-42351895/ny/white-plains/snarr-northeast-ny1298/#story")
+# Start page to scrape
+#driver.get("https://www.petfinder.com/search/dogs-for-adoption/us/ny/white-plains/?distance=10&sort%5B0%5D=recently_added")
+# Do I need to fill-in search form to get set of pets to scrape??
 
-# Click dog's pic to go to its page
+#TEST SITES FOR SCRAPING AN INDIVIDUAL PET PAGE:
+#driver.get("https://www.petfinder.com/dog/cliff-42127137/ny/new-york/shelter-chic-ny1286/") #links below don't work
+driver.get("https://www.petfinder.com/dog/brody-42351895/ny/white-plains/snarr-northeast-ny1298/#story")  #links below work
+#driver.get("https://www.petfinder.com/dog/celine-dion-42077584/ny/new-york/shelter-chic-ny1286/")
+
+#STEPS:
+# 1 - From search result page - Click dog's pic to go to its page
+# 2 - Click 2nd time on photo
+# 3 - Scrape individual pages
+# 4 - Click "Next Pet"
+# 5 - Repeat 3 & 4 until no more pages
+
 # pet_tile = driver.find_element_by_xpath('//a[@class="petCard-link"]')
 # pet_tile.click()
 
-
+# Sample code & notes from Zeyu:
 # Page index used to keep track of where we are.
 # index = 1
 # # We want to start the first two pages.
@@ -23,43 +33,64 @@ driver.get("https://www.petfinder.com/dog/brody-42351895/ny/white-plains/snarr-n
 # #		index = index + 1
 # 		# Find all the dogs on page. The find_elements function will return a list of selenium select elements.
 # 		# Check the documentation here: http://selenium-python.readthedocs.io/locating-elements.html
-# 		pets = driver.find_elements_by_xpath('//div[@itemprop="review"]')
+#
+#       pets = driver.find_elements_by_xpath('//div[@itemprop="review"]')
 # 		# Iterate through the list and find the details of each review.
 # 		for pet in pets:
 # 			# Initialize an empty dictionary for each pet
 #			pet_dict = {}
-# 			# Use relative xpath to locate the title, content, username, date, rating.
-# 			# Once you locate the element, you can use 'element.text' to return its string.
-# 			# To get the attribute instead of the text of each element, use `element.get_attribute()`
-# 			title = review.find_element_by_xpath('.//div[@itemprop="headline"]').text
-# 			# Your code here
 
+#####
 pet_dict = {}
 name = driver.find_element_by_xpath('.//h1[@id="Detail_Main"]').text
-testcase = driver.find_element_by_css_selector('#Site > div > div > div.tier.tier_flush\40 maxLg.u-vr8x.u-vrTop6x > div > div.grid-col.grid-col_2\2f 3\40 minLg > div > div:nth-child(1) > div > div.hrTitle.u-isHidden\40 minLg.u-vr3x > span > span > ul > li:nth-child(2)').text
-#Site > div > div > div.tier.tier_flush\40 maxLg.u-vr8x.u-vrTop6x > div > div.grid-col.grid-col_2\2f 3\40 minLg > div > div:nth-child(1) > div > div.hrTitle.u-isHidden\40 minLg.u-vr3x > span > span > ul > li:nth-child(2)
-#animal = driver.find_element_by_xpath('(.//ul[@class="hrArray hrArray_bulletDivided u-vr4x"])')
-#breed = driver.find_element_by_xpath('.//span[@class="txt m-txt_bold m-txt_lg m-txt_colorPrimaryS2"]/li').text
-age_range = driver.find_element_by_xpath('//span[@class="txt m-txt_lg m-txt_colorPrimaryS2"]').text
-#testcase = driver.find_element_by_xpath('.//span[@class="hrTitle-body"]')
+organization = driver.find_element_by_xpath('.//pf-truncate[@line-count="3"]').text
+contact_email =  driver.find_element_by_xpath('(.//a[@class="txt txt_link m-txt_bold"])[2]').text
+story = driver.find_element_by_xpath('(.//div[@class="u-vr4x"])[2]').text
+#     long description
 
-#gender =  driver.find_element_by_xpath('//span[contains(text(),'Male')]').text
-#size = driver.find_element_by_xpath('//span[@class="txt m-txt_lg m-txt_colorPrimaryS2"]')
-# colors =
-coat_length = driver.find_element_by_xpath('.//dd[@class="txt u-vr4x"][2]').text
-health = driver.find_element_by_xpath('.//dd[@class="txt"]').text
-characteristics = driver.find_element_by_xpath('.//dd[@class="txt u-vr4x"]').text
-house_train = driver.find_element_by_xpath('.//dd[@class="txt u-vr4x"][3]').text
-# good_with =
-# location = driver.find_element_by_xpath('//span[@class="txt m-txt_bold m-txt_lg m-txt_colorPrimaryS2"]').text
-# story =
-# contact_info =
-# list_date = driver.find_element_by_link_text("published_at")
-# photo_link =
+info1 = driver.find_element_by_xpath('.//div[@class="card-section-inner"]').text
+#     name, breed, location, age_range, gender, size
 
-#print("name=",name, "age=",age_range, "health=",health, "char=",characteristics)
-#print("coatlength=",coat_length,"housetrain=",house_train)
-print("--> testing==",testcase)
+info2 = driver.find_element_by_xpath('//div[@class="grid grid_gutterLg u-vr4x"]').text
+#     house_train, health, good_with
+
+info3 = driver.find_element_by_xpath('//ul[@aria-label="Pet physical characteristics"]').text
+#     age_range, gender, size, colors
+
+info4 = driver.find_element_by_xpath('//div[@class="grid-col grid-col_1/2@minMd grid-col_1/1@minLg"]').text
+#     char, coat, house-train, health
+
+info5 = driver.find_element_by_xpath('(.//div[@class="grid-col grid-col_1/2@minMd grid-col_1/1@minLg"])[2]').text
+#     good_with, adoption fee
+
+#testcase = driver.find_element_by_xpath('(.//a[@class="txt txt_link m-txt_bold"])[2]').text
+#driver.find_elements_by_xpath("(.//script[contains(text(), 'dimension1')])[4]")
+#driver.find_element_by_xpath('//script[@type="text/javascript"]/script').getAttribute("script")
+#driver.find_element_by_xpath('*').text
+
+# animal = driver.find_element_by_xpath('(.//ul[@class="hrArray hrArray_bulletDivided u-vr4x"])')
+# list_date = driver.find_element_by_link_text("published_at") #buried in script
+# photo_link = ??
+
+pet_dict['name'] = name
+pet_dict['organization'] = organization
+pet_dict['contact_email'] = contact_email
+pet_dict['info1'] = info1
+pet_dict['info2'] = info2
+pet_dict['info3'] = info3
+pet_dict['info4'] = info4
+pet_dict['info5'] = info5
+pet_dict['story'] = story
+print(pet_dict.values())
+# writer.writerow(pet_dict.values())
+
+
+######Print Out Variables to Check Results: #####
+#print("--> testing==",testcase)
+
+
+
+
 		# # Locate the next button element on the page and then call `button.click()` to click it.
 		# button = driver.find_element_by_xpath('//button[@class="fieldBtn fieldBtn_altHover m-fieldBtn_iconRt m-fieldBtn_tight m-fieldBtn_full"]')
 		# button.click()
